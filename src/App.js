c999edd5386e956ff191405ce39341a363d1e614
Ajay Parameshwar Morade <103033106+AjayMorde@users.noreply.github.com>
2023-08-29 02:00:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Header from './components/Header/Header';
 import ResultsTable from './components/ResultsTable/ResultsTable';
 import UserInput from './components/UserInput/UserInput';
-
+ 
 function App() {
   const [userInput, setUserInput] = useState(null);
 
